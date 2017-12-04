d485013e2c1ecba0efa867e9793639e7d1603c31
Diogo Simão <diogosimao@gmail.com>
2017-12-04 17:47:48 -0200

    Just a small change on __str__ model method

--- apps/branches/models.py
+++ apps/branches/models.py
@@ -3,14 +3,15 @@ from bakery.core.models import TimestampedModel
 
 
 class Branch(TimestampedModel):
+    description = models.CharField(max_length=255, blank=True)
     address = models.CharField(unique=True, max_length=200, blank=False)
     city = models.CharField(max_length=100, blank=False)
     state = models.CharField(max_length=50, blank=False)
 
     def __str__(self):
-        return self.address
+        return self.description if self.description else self.address
 
     class Meta:
         ordering = ('address',)
-        verbose_name_plural = "branches"
+        verbose_name_plural = 'branches'
 
