3a22995ad70c5bed150a3a789a47c1213b5fc8ff
Diogo Simão <diogosimao@gmail.com>
2017-12-04 22:32:52 -0200

    Nested route for payment

--- apps/bills/urls.py
+++ apps/bills/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
-from .views import BillViewSet
+from rest_framework_nested import routers
+
+from .views import BillViewSet, PaymentViewSet
 
 
-router = DefaultRouter()
-router.register(r'', BillViewSet, base_name='bills')
+router = DefaultRouter(trailing_slash=True)
+router.register(r'bills', BillViewSet, base_name='bills')
+
+bills_router = routers.NestedSimpleRouter(router, r'bills', lookup='bill', )
+bills_router.register(r'payment', PaymentViewSet, base_name='bill-payment')
 
 
 schema_view = get_schema_view(title='Bakery bills API')
@@ -13,5 +18,6 @@ schema_view = get_schema_view(title='Bakery bills API')
 urlpatterns = [
     url(r'^schema/$', schema_view),
     url(r'^', include(router.urls)),
+    url(r'^', include(bills_router.urls)),
 ]
 
